d6004797dd8116414a4190867dacd95a69d02218
sandeepsidmec <sandeep@sidmectech.in>
2025-04-11 11:20:29 +0530

    second commit

--- hospital_management/__manifest__.py
+++ hospital_management/__manifest__.py
@@ -3,10 +3,12 @@
     "author": "sandeep",
     "version": "18.0",
     "license": "LGPL-3",
+    "depends": ["sale", "sale_management", "mail",],
     "data":
         [
+
             "security/ir.model.access.csv",
             "views/view_patient.xml",
+            "views/view_sale_order.xml",
             "views/menu.xml", ],
-
 }
--- hospital_management/models/patient.py
+++ hospital_management/models/patient.py
@@ -1,10 +1,10 @@
-from odoo import models,fields
+from odoo import models, fields
 
 
 class HospitalPatient(models.Model):
     _name = "hospital.patient"
+    _rec_name = "patient_name"
+    _inherit = ['mail.thread', 'mail.activity.mixin']
 
-    patient_name=fields.Char(string="Name")
-    age=fields.Integer(string="Age")
-
-
+    patient_name = fields.Char(string="Name", tracking=True)
+    age = fields.Integer(string="Age")
