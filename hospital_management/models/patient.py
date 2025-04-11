4702800aaa793bf151bc399b2547bd9e15c5e550
sandeepsidmec <sandeep@sidmectech.in>
2025-04-11 13:00:49 +0530

    updated

--- hospital_management/models/patient.py
+++ hospital_management/models/patient.py
@@ -3,8 +3,15 @@ from odoo import models, fields
 
 class HospitalPatient(models.Model):
     _name = "hospital.patient"
-    _rec_name = "patient_name"
+    _rec_name = "patient_id"
     _inherit = ['mail.thread', 'mail.activity.mixin']
 
-    patient_name = fields.Char(string="Name", tracking=True)
+    patient_id = fields.Many2one(comodel_name="res.partner", string="Name", tracking=True, required=True, )
     age = fields.Integer(string="Age")
+    gender = fields.Selection([("male", "Male"), ("female", "Female")], "Gender")
+    is_patient_in_ward = fields.Boolean("is patient in ward")
+    admit_date = fields.Date("Admit date")
+    discharge_date = fields.Date("discharge Date")
+
+    patient_names = fields.Many2many(comodel_name="res.partner", string="Names")
+
