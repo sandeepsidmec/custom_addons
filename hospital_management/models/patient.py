47e0a98b32be9de100d029465d94f0d6f82f21f6
sandeepsidmec <sandeep@sidmectech.in>
2025-04-14 11:07:55 +0530

    notebook and pages

--- hospital_management/models/patient.py
+++ hospital_management/models/patient.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class HospitalPatient(models.Model):
@@ -6,10 +6,41 @@ class HospitalPatient(models.Model):
     _rec_name = "patient_id"
     _inherit = ['mail.thread', 'mail.activity.mixin']
 
-    patient_id = fields.Many2one(comodel_name="res.partner", string="Name", tracking=True, required=True, )
+    patient_id = fields.Many2one(comodel_name="res.partner", domain=[("email", "!=", False)], string="Name",
+                                 tracking=True, required=True, stored=True)
+    email = fields.Char(related="patient_id.email", string="email")
     age = fields.Integer(string="Age")
     gender = fields.Selection([("male", "Male"), ("female", "Female")], "Gender")
     is_patient_in_ward = fields.Boolean("is patient in ward")
     admit_date = fields.Date("Admit date")
     discharge_date = fields.Date("discharge Date")
     doctor_names = fields.Many2many(comodel_name="hospital.doctor", string="Doctors")
+    date = fields.Date("Date")
+    patient_lines = fields.One2many("hospital.patient.line", "patient", "Order lines")
+
+    user_id = fields.Many2one("res.users", "user", compute="compute_user_company")
+    company_id = fields.Many2one("res.company", "Company", compute="compute_user_company")
+
+    # @api.onchange("patient_id")
+    # def onchange_patient_name(self):
+    #     for rec in self:
+    #         print(rec)
+    #         rec.email = rec.patient_id.email
+
+    # def compute_patient_email(self):
+    #     for rec in self:
+    #         rec.email = rec.patient_id.email
+
+    def compute_user_company(self):
+        for rec in self:
+            rec.user_id = self.env.user
+            rec.company_id = self.env.user.company_id.id
+
+
+class HospitalPatientLines(models.Model):
+    _name = "hospital.patient.line"
+
+    product_id = fields.Many2one("product.product", "product Name")
+    qty = fields.Integer("qty")
+    unit_price = fields.Float("Unit price")
+    patient = fields.Many2one("hospital.patient", "patient")
