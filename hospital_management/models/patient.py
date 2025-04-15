fdf3b5a53ac386844ac99ef9c25f4486cf437b65
sandeepsidmec <sandeep@sidmectech.in>
2025-04-15 11:55:21 +0530

    added

--- hospital_management/models/patient.py
+++ hospital_management/models/patient.py
@@ -1,4 +1,4 @@
-from odoo import models, fields, api
+from odoo import models, fields
 
 
 class HospitalPatient(models.Model):
@@ -21,6 +21,8 @@ class HospitalPatient(models.Model):
     user_id = fields.Many2one("res.users", "user", compute="compute_user_company")
     company_id = fields.Many2one("res.company", "Company", compute="compute_user_company")
 
+    status = fields.Selection([("admit", "Admitted"), ("discharge", "Discharged")], "status", default='admit')
+
     # @api.onchange("patient_id")
     # def onchange_patient_name(self):
     #     for rec in self:
@@ -36,6 +38,22 @@ class HospitalPatient(models.Model):
             rec.user_id = self.env.user
             rec.company_id = self.env.user.company_id.id
 
+    def send_email(self):
+        for rec in self:
+            template = self.env.ref("hospital_management.mail_template_patient_confirm")
+            template.send_mail(rec.id, force_send=True)
+
+    def view_patient_lines(self):
+        self.ensure_one()
+        for rec in self:
+            return {
+                'name': "view patient invoices",
+                'view_mode': 'list',
+                'res_model': 'hospital.patient.line',
+                'domain': [('patient', '=', rec.patient_id.id)],
+                'type': 'ir.actions.act_window',
+            }
+
 
 class HospitalPatientLines(models.Model):
     _name = "hospital.patient.line"
@@ -43,4 +61,9 @@ class HospitalPatientLines(models.Model):
     product_id = fields.Many2one("product.product", "product Name")
     qty = fields.Integer("qty")
     unit_price = fields.Float("Unit price")
+    subtotal = fields.Float("sub-total", compute="subtotal")
     patient = fields.Many2one("hospital.patient", "patient")
+
+    def subtotal(self):
+        for i in self:
+            i.subtotal = i.qty * i.unit_price
